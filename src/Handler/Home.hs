aa83e7bba74b983f2db29d71f5e16a1013285444
parsonsmatt <parsonsmatt@gmail.com>
2018-09-11 23:03:00 -0600

    god i hate javascript

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -4,12 +4,16 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Handler.Home where
 
 import Import
 
-import Yesod.Table hiding (maybe)
+import qualified Data.Set as Set
+import Yesod.Table hiding (maybe, show)
 import qualified Text.Blaze as Blaze
+
+import Database.Persist.Sql
 
 getHomeR :: Handler Html
 getHomeR = do
@@ -23,6 +27,7 @@ getSpellIndexR = do
     let spellList = spellTable spells
     defaultLayout $ do
         setTitle "Spellcards"
+        setUltDestCurrent
         $(widgetFile "spell-index")
 
 spellTable :: [Entity Spell] -> Widget
@@ -33,6 +38,12 @@ spellTable = buildBootstrap $ mconcat
     , text "Descriptor" (spellDescriptor . entityVal)
     , text "Spell Resistance" (spellSpellResistance . entityVal)
     , text "Reference" (spellReference . entityVal)
+    , widget "Toggle Spell" (\(Entity spellId _) ->
+        [whamlet|
+            <button data-spell-id=#{tshow (fromSqlKey spellId)} .btn-large .toggle-button>
+                Toggle
+        |]
+        )
     ]
 
 getSpellR :: SpellId -> Handler Html
@@ -43,3 +54,43 @@ getSpellR spellId = do
             "Spellcards - "
             Blaze.text spellName
         $(widgetFile "spell-show")
+
+getSpellListR :: Handler Html
+getSpellListR = do
+    spellList <- readSpellList
+    defaultLayout $ [whamlet|
+        #{tshow spellList}
+    |]
+
+putUpdateSpellListR :: SpellId -> Handler ()
+putUpdateSpellListR spellId = do
+    addSpell spellId
+    redirectUltDest HomeR
+
+deleteUpdateSpellListR :: SpellId -> Handler ()
+deleteUpdateSpellListR spellId = do
+    removeSpell spellId
+    redirectUltDest HomeR
+
+addSpell :: SpellId -> Handler ()
+addSpell spellId = do
+    spellList <- readSpellList
+    writeList (Set.insert spellId spellList)
+
+removeSpell :: SpellId -> Handler ()
+removeSpell spellId = do
+    spellList <- readSpellList
+    writeList (Set.delete spellId spellList)
+
+readSpellList :: Handler (Set SpellId)
+readSpellList = do
+    mlist <- lookupSession "spell-list"
+    case mlist >>= readMay of
+        Nothing -> do
+            writeList mempty
+            pure mempty
+        Just list ->
+            pure (Set.fromList (map toSqlKey list))
+
+writeList :: Set SpellId -> Handler ()
+writeList = setSession "spell-list" . pack . show . map fromSqlKey . Set.toList
